1c4b75979382bbb11c7606af4f4c276448612ad8
kartozia <inga.kartoziya@gmail.com>
2017-02-26 00:07:15 +0100

    working wow

--- HW2/HW2_Kartozia.py
+++ HW2/HW2_Kartozia.py
@@ -1,26 +1,6 @@
 #Language Detection, Kartozia
 import wikipedia
 import re
-##nl_file = open('nl_ngram.txt', 'r', encoding='utf-8')
-##bg_file = open('bg_ngram.txt', 'r', encoding='utf-8')
-##es_file = open('es_ngram.txt', 'r', encoding='utf-8')
-##hu_file = open('hu_ngram.txt', 'r', encoding='utf-8')
-nl_freq = open('nl_freq.txt', 'r', encoding='utf-8')
-bg_freq = open('bg_freq.txt', 'r', encoding='utf-8')
-es_freq = open('es_freq.txt', 'r', encoding='utf-8')
-hu_freq = open('hu_freq.txt', 'r', encoding='utf-8')
-
-nl = nl_freq.readlines()
-##nl1 = nl_file.readlines()
-
-bg = bg_freq.readlines()
-##bg1 = bg_file.readlines()
-
-es = es_freq.readlines()
-##es1 = es_file.readlines()
-
-hu = hu_freq.readlines()
-##hu1 = hu_file.readlines()
 
 def get_texts_for_lang(lang, n=10): # функция для скачивания статей из википедии
     wikipedia.set_lang(lang)
@@ -39,72 +19,105 @@ def get_texts_for_lang(lang, n=10): # функция для скачивания
 
 wiki_texts = {}
 for lang in ('nl', 'bg', 'es', 'hu', 'ru', 'de'): # испанский, венгерский, нидерландский и болгарский, русский и немецкий 
-    wiki_texts[lang] = get_texts_for_lang(lang, 2)
+    wiki_texts[lang] = get_texts_for_lang(lang, 100)
     print(lang, len(wiki_texts[lang]))
     
 def lang_check(text, language):
     lang = language.read()
     lang = lang.split()
-    print(lang)
     count = 0
     for l in lang:
-        if l.strip() in text:
-            count += 1
+        if l not in '0123456789':
+            if l in text:
+                count += 1
     percentage = (count/len(lang))*100
     return percentage
 
-def whether_correct(lang, name, guess, answer):
-    if (lang == guess) and (name == answer):
-        return name +': ' + lang + 'Correct'
-    elif (lang == guess) and (name != answer):
-        return name +': ' + lang + 'Wrong'
-        
-
+       
+#ngram method
 def ngram_detect(key, value):
     correct = 0
     nl_file = open('nl_ngram.txt', 'r', encoding='utf-8')
     bg_file = open('bg_ngram.txt', 'r', encoding='utf-8')
     es_file = open('es_ngram.txt', 'r', encoding='utf-8')
     hu_file = open('hu_ngram.txt', 'r', encoding='utf-8')
-    nl = lang_check(value, nl_file)
-    bg = lang_check(value, bg_file)
-    es = lang_check(value, es_file)
-    hu = lang_check(value, hu_file)
-    if nl == max(nl, bg, es, hu):
-        if 'nl' == key:
-            correct += 1
-            return 'Dutch: ' + str(nl) + '% Correct'
-        else:
-            return 'Dutch: ' + str(nl) + '% Wrong'
-    elif bg == max(nl, bg, es, hu):
-        if 'bg' == key:
-            correct += 1
-            return 'Bulgarian: ' + str(bg) + '% Correct'
-        else:
-            return 'Bulgarian ' + str(bg) + '% Wrong'        
-    elif es == max(nl, bg, es, hu):
-        if 'es' == key:
-            correct += 1
-            return 'Spanish: ' + str(es) + '% Correct'        
-        else:
-            return 'Spanish: ' + str(es) + '% Wrong'
-    elif hu == max(nl, bg, es, hu):
-        if 'hu' == key:
-            correct += 1
-            return 'Hungarian: ' + str(hu) + '% Correct'    
-        else:
-            return 'Hungarian: ' + str(hu) + '% Wrong'
-    print (correct)
+    for v in value:
+        hu = lang_check(v, hu_file)
+        es = lang_check(v, es_file)
+        nl = lang_check(v, nl_file)
+        bg = lang_check(v, bg_file)
+        if nl == max(nl, bg, es, hu):
+            if 'nl' == key:
+                correct += 1
+                return 'Ngram: Dutch: ' + str(nl) + '% Correct' 
+            else:
+                return 'Ngram: Dutch: ' + str(nl) + '% Wrong'       
+        if es == max(nl, bg, es, hu):
+            if 'es' == key:
+                correct += 1
+                return 'Ngram: Spanish: ' + str(es) + '% Correct'        
+            else:
+                return 'Ngram: Spanish: ' + str(es) + '% Wrong'
+        if hu == max(nl, bg, es, hu):
+            if 'hu' == key:
+                correct += 1
+                return 'Ngram: Hungarian: ' + str(hu) + '% Correct'    
+            else:
+                return 'Ngram: Hungarian: ' + str(hu) + '% Wrong'
+        if bg == max(nl, bg, es, hu):
+            if 'bg' == key:
+               correct += 1
+               return 'Ngram: Bulgarian: ' + str(bg) + '% Correct'
+            else:
+               return 'Ngram: Bulgarian ' + str(bg) + '% Wrong'
+    nl_file.close()
+    es_file.close()
+    bg_file.close()
+    hu_file.close()
+
+#dic method
+def freq_detect(key, value):
+    correct = 0
+    nl_freq = open('nl_freq.txt', 'r', encoding='utf-8')
+    bg_freq = open('bg_freq.txt', 'r', encoding='utf-8')
+    es_freq = open('es_freq.txt', 'r', encoding='utf-8')
+    hu_freq = open('hu_freq.txt', 'r', encoding='utf-8')
+    for v in value:
+        hu = lang_check(v, hu_freq)
+        es = lang_check(v, es_freq)
+        nl = lang_check(v, nl_freq)
+        bg = lang_check(v, bg_freq)
+        if nl == max(nl, bg, es, hu):
+            if 'nl' == key:
+                correct += 1
+                return 'Dic: Dutch: ' + str(nl) + '% Correct' 
+            else:
+                return 'Dic: Dutch: ' + str(nl) + '% Wrong'       
+        if es == max(nl, bg, es, hu):
+            if 'es' == key:
+                correct += 1
+                return 'Dic: Spanish: ' + str(es) + '% Correct'        
+            else:
+                return 'Dic: Spanish: ' + str(es) + '% Wrong'
+        if hu == max(nl, bg, es, hu):
+            if 'hu' == key:
+                correct += 1
+                return 'Dic: Hungarian: ' + str(hu) + '% Correct'    
+            else:
+                return 'Dic: Hungarian: ' + str(hu) + '% Wrong'
+        if bg == max(nl, bg, es, hu):
+            if 'bg' == key:
+               correct += 1
+               return 'Dic: Bulgarian: ' + str(bg) + '% Correct'
+            else:
+               return 'Dic: Bulgarian ' + str(bg) + '% Wrong'
+    nl_freq.close()
+    es_freq.close()
+    bg_freq.close()
+    hu_freq.close()
+
+for key, value in wiki_texts.items():
+    print(ngram_detect(key,value))
+    print(freq_detect(key, value))
     
-for key,value in wiki_texts.items():
-    print(ngram_detect(key, value))
-    
-#def freq_detect(text):
-nl_freq.close()
-es_freq.close()
-bg_freq.close()
-hu_freq.close()
-##nl_file.close()
-##es_file.close()
-##bg_file.close()
-##hu_file.close()
+
